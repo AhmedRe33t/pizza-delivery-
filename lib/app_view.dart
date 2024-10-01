6b5a08cc35798494a60b8d10e274572638a1845a
ahmedrefaat <arrefaat171@gmail.com>
2024-10-02 00:58:21 +0300

    them color

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,13 +1,31 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza/bloc/AuthanticationBloc/authantication_bloc_bloc.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Food delivary',
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        )
+       ),
+       home: BlocBuilder<AuthanticationBloc,AuthenticationState>(
+        builder:(context, state) {
+          if(state.status==AuthenticationStatus.authentIcated){
+            return Container();
+          }else {
+            return Container();
+          }
+        },),
     );
   }
 }

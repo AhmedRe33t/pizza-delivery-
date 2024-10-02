4f1f0c1b26681d3adb0a60a87f671f39603b64b5
ahmedrefaat <arrefaat171@gmail.com>
2024-10-02 08:45:54 +0300

    finish details view and home view

---
+++ lib/screens/home/views/details_view.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pizza/componant/macro.dart';
+import 'package:pizza/constants/assets.dart';
+
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: const Color.fromARGB(255, 232, 237, 238),
+      appBar:AppBar(
+        backgroundColor:  const Color.fromARGB(255, 232, 237, 238), 
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Container(
+              width: MediaQuery.of (context). size.width,
+              height: MediaQuery.of (context). size.width-(40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow:const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(3, 3),
+                    blurRadius: 10
+                  )
+                ],
+                image: DecorationImage(image: AssetImage(Assets.images4))
+
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Container(
+                
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow:const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(3, 3),
+                      blurRadius: 10
+                    )
+                  ],),
+                  child:  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child:   Column(
+                      children: [
+                      const Row(
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: Text('Truffle Temptation Extravaganza',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold
+                              ),),
+                            ),
+                              Expanded(
+                              flex: 1,
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Column(
+                                  children: [
+                                    Text('10.00\$',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                       color:  Color.fromARGB(255, 8, 126, 210),
+                                    ),),
+                                      Text('15.00\$',
+                                     
+                                         style: TextStyle(
+                                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.w700,
+                          color:  Colors.grey,
+                          fontSize: 15
+                                         ),),
+                                  ],
+                                ),
+                                
+
+                              ),
+                            ),
+                                
+                          ],
+                        ),
+                       const SizedBox(height: 12,),
+                        const Row(
+                          children: [
+                             MyMacroWidget(title: 'Calories', value: 233, icon: FontAwesomeIcons.fire,),
+                              SizedBox(width: 10,),
+                           MyMacroWidget(title:'Protein', value: 233, icon: FontAwesomeIcons.dumbbell),
+                              SizedBox(width: 10,),
+                              MyMacroWidget(title: 'Fat', value: 233, icon: FontAwesomeIcons.bowlFood,),
+                           SizedBox(width: 10,),
+                               MyMacroWidget(title: 'Carbs', value: 233, icon: CupertinoIcons.chart_bar_fill,),
+                          ],
+                        ),
+
+                         const SizedBox(height: 40,),
+                         SizedBox(
+                          
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          child: TextButton(onPressed: (){}, 
+                           style: TextButton.styleFrom(
+                      elevation: 3.0,
+                      backgroundColor: Colors.black, 
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      )
+                    ),
+                          child:const Text('By now',
+                           style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600
+                        ),)),
+                         )
+                      ],
+                    ),
+                  ),
+                  
+                  ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,10 +1,149 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza/constants/assets.dart';
+import 'package:pizza/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:pizza/screens/home/views/details_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      backgroundColor: const Color.fromARGB(255, 232, 237, 238),
+      appBar:AppBar(
+        backgroundColor: const Color.fromARGB(255, 232, 237, 238),
+        title: Row(
+          children: [
+            Image.asset(Assets.images8,scale: 14,),
+           const SizedBox(width: 8,),
+          const  Text('PIZZA',style: TextStyle(fontSize: 30,fontWeight: FontWeight.w900),)
+          ],
+        ),
+        actions: [
+         
+           IconButton(onPressed: (){}, icon:const Icon(CupertinoIcons.cart,)),
+            IconButton(onPressed: (){
+            context.read<SignInBloc>().add(SignOutRequired());
+          }, icon:const Icon(CupertinoIcons.arrow_right_to_line,)),
+          
+        ],
+      ) ,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount:2 ,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            childAspectRatio: 9/16
+            ),
+            itemCount: 8,
+             
+          itemBuilder:(context, index) {
+            return Material(
+              elevation: 3,
+              color: Colors.white,
+              shape: RoundedRectangleBorder (
+                borderRadius: BorderRadius. circular (20),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius. circular (20),
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder:(context) =>const DetailsScreen(),));
+                    },
+                child: Container(
+               
+                 child: Column(
+                  children: [
+                    Image.asset(Assets.images2),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 4),
+                              child: Text('Non_veg ',style: TextStyle(color: Colors.white,fontSize: 10,fontWeight: FontWeight.bold),),
+                            ),
+                          ),
+                   const SizedBox(width: 8,),
+                           Container(
+                            decoration: BoxDecoration(
+                              color: Colors.green.withOpacity(.2),
+                              borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 4),
+                              child: Text('🌶️ BALANCE ',style: TextStyle(color: Colors.green,fontSize: 10,fontWeight: FontWeight.w900),),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8,),
+                 const   Padding(padding: EdgeInsets.symmetric(horizontal: 12),
+                    child: Text('Cheesy marvel',
+                   // textAlign: TextAlign.start,
+                    style: TextStyle(
+                      
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    ),),
+                     ),
+                       const SizedBox(height: 3,),
+                    Padding(padding:const EdgeInsets.symmetric(horizontal: 12),
+                    child:
+                     Text('Crafting joy: your pizza, your rules, best taste!',
+                 
+                    style: TextStyle(
+                      
+                      color: Colors.grey.shade700,
+                      fontSize: 10
+                    ),),
+                     ),
+                     Padding(padding: const EdgeInsets.symmetric(horizontal: 12),
+                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                       children: [
+                      const   Row(
+                         
+                          children: [
+                                        Text('10.00\$',
+                                     
+                                         style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color:  Color.fromARGB(255, 8, 126, 210),
+                          fontSize: 16
+                                         ),),
+                                         SizedBox(width: 5,),
+                                         Text('15.00\$',
+                                     
+                                         style: TextStyle(
+                                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.w700,
+                          color:  Colors.grey,
+                          fontSize: 12
+                                         ),),
+                          ],
+                         ),
+                                                              IconButton(onPressed: (){}, icon:const Icon(CupertinoIcons.add_circled_solid,color: Colors.black,))
+                
+                       ],
+                     ),)
+                  ],
+                 ),
+                ),
+              ),
+            );
+          },),
+      ),
+    );
   }
 }
